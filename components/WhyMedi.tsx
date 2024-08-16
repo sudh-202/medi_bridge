4b6134c027060069113cc0270dad502a80173523
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-16 22:22:01 +0530

    the changes

--- components/WhyMedi.tsx
+++ components/WhyMedi.tsx
@@ -20,23 +20,24 @@ const WhyMedi = () => {
                     </div>
                 </div>
         */
-
-        <main className='flex flex-row items-center justify-center gap-10 translate-x-[10%]'>
-            <div className='w-1/2'>
-                <Carousel />
-            </div>
-            <div className='w-1/2 flex flex-col'>
-                <div className="bg-white rounded-3xl px-10 py-5  mb-4 border-[#C9D851] border-2 w-1/2">
-                    <Image src="/3steps.webp" alt="MediBridge Building" className="rounded-md mb-2 w-full h-auto" width={500} height={100} />
-                    <button className="bg-[#0F3C49] text-white px-4 py-3  rounded-xl w-full mt-[-25px]">Order now</button>
-                </div>
+        <div>
+            <main className='flex flex-row items-center justify-center gap-10 translate-x-[10%]'>
                 <div className='w-1/2'>
-                <Image src="/medibuild.webp" alt="MediBridge Building" className="" width={500} height={100} />
+                    <Carousel />
                 </div>
+                <div className='w-1/2 flex flex-col'>
+                    <div className="bg-white rounded-3xl px-10 py-5  mb-4 border-[#C9D851] border-2 w-1/2">
+                        <Image src="/3steps.webp" alt="MediBridge Building" className="rounded-md mb-2 w-full h-auto" width={500} height={100} />
+                        <button className="bg-[#0F3C49] text-white px-4 py-3  rounded-xl w-full mt-[-25px]">Order now</button>
+                    </div>
+                    <div className='w-1/2'>
+                        <Image src="/medibuild.webp" alt="MediBridge Building" className="" width={500} height={100} />
+                    </div>
+                </div>
+            </main>
+            <Image src="/news1.webp" alt='news1' width={1200} height={100} className=' mx-auto my-20'/>
+            <Image src="/letter.webp" alt='news1' width={1000} height={100} className=' mx-auto my-20'/>
             </div>
-
-
-        </main>
     );
 }
 
