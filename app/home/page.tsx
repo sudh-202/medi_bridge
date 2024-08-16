41e5b156719aba873b9b03587e3a76e169943713
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-08-16 22:12:50 +0530

    this is

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
     <ImageSlider images={images}/>
     <WellnessCard />
     <PackageCard/>
-    <VitalOrgansSection/>
-    <MergedCarousel/>
+    <VitalOrgansSection/> 
     <Recommendation/>
+    <MergedCarousel/>
     <IconV1/>
     <Services/>
     <WhyMedi/>
